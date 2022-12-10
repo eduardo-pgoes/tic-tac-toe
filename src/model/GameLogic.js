f00fbe98d707949e8df14ac74b6566a47488d24d
Eduardo Goes <e.goes@unifesp.br>
2022-12-10 09:48:04 -0300

    📝 add game logic documentation

--- src/model/GameLogic.js
+++ src/model/GameLogic.js
@@ -24,13 +24,17 @@ class GameLogic {
         return undefined;
     }
 
+    /* Sets the game's state. */
     setGameState(gameState) {
         this.gameState = gameState;
     }
 
-    /*
-        Verifies
-    */
+    /* Adds a piece to the board - if it's a valid one. */
+    addPiece(piece, row, column) {
+        if (piece === 'O' || piece === 'X') this.gameState[row[column]] = piece;
+    }
+
+    /* Verifies a victory in the main diagonal. */
     #verifyMainDiagonal(gameState) {
         if ((gameState[0][0] === gameState[1][1]) && (gameState[1][1] === gameState[2][2])) {
             return gameState[0][0];
@@ -38,6 +42,7 @@ class GameLogic {
         return undefined;
     }
 
+    /* Verifies a victory in the anti diagonal. */
     #verifyAntiDiagonal(gameState) {
         if ((gameState[0][2] === gameState[1][1]) && (gameState[1][1] === gameState[2][0])) {
             return gameState[0][2] !== '' ? gameState[0][2] : undefined;
@@ -45,6 +50,7 @@ class GameLogic {
         return undefined;
     }
 
+    /* Verifies a victory through lines */
     #verifyLines(gameState) {
         for (let i = 0; i < 3; i++) {
             if ((gameState[i][0] === gameState[i][1]) && (gameState[i][1] === gameState[i][2])) {
@@ -54,6 +60,7 @@ class GameLogic {
         return undefined;
     }
 
+    /* Verifies a victory through columns. */
     #verifyColumns(gameState) {
         for (let i = 0; i < 3; i++) {
             if ((gameState[0][i] === gameState[1][i]) && (gameState[1][i] === gameState[2][i])) {
